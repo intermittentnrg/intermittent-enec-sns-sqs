6d2d58110e5758ff089b2a29b6d59d4c06b28767
intermittent.energy <git@intermittent.energy>
2026-02-15 16:29:02 +0100

    Add import GitHub action

---
+++ receive.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+require 'bundler/setup'
+require 'aws-sdk-sqs'
+require 'json'
+
+queue_url = "https://sqs.me-central-1.amazonaws.com/115154022797/enec-sns-sqs-github"
+sqs = Aws::SQS::Client.new(region: 'me-central-1')
+paths = []
+receipt_handles = []
+
+loop do
+  receive_message_result = sqs.receive_message({
+    queue_url: queue_url,
+    max_number_of_messages: 10,
+    wait_time_seconds: 0 # Do not wait to check for the message.
+  })
+  receive_message_result.messages.each do |message|
+    receipt_handles << message.receipt_handle
+    body = message.body
+    doc = Nokogiri::HTML(body)
+    script = doc.at_css('script:contains("actualDate")')&.text
+    script =~ /let actualDate = new Date\("([^"]+)"\)/
+    path = ::Regexp.last_match(1).gsub(/\s/, '/')
+
+    dir = File.dirname(path)
+    Dir.mkdir(dir) unless Dir.exist?(dir)
+    path = "#{path}.html"
+    File.write(path, message.body)
+    puts path
+
+    paths << path
+  rescue JSON::ParserError
+    puts $!
+  end
+  break if receive_message_result.messages.length <10
+end
+
+raise 'no files' if paths.empty?
+
+system "git", "add", "-v", *paths
+system "git", "commit", "-m", "Updated data from AWS SQS"
+system "git", "push", exception: true
+
+i=0
+receipt_handles.each_slice(10) do |batch|
+  sqs.delete_message_batch({
+    queue_url: queue_url,
+    entries: batch.map do |receipt_handle|
+      {
+        id: (i += 1).to_s,
+        receipt_handle:
+      }
+    end
+  })
+  puts "delete"
+end
